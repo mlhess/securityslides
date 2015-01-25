9a44287edd116f214a9a8af538023ebe419c3a9c
Michael Hess <mlhess@umich.edu>
2015-01-25 13:58:16 -0500

    first commit

---
+++ customcss/config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'zen-grids'
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
